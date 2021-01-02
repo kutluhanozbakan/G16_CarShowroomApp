43f5a34118dacab22b1a115bbe69ac066eeebfdd
kutluhanozbakan <kutluhanozbakan21@gmail.com>
2021-01-02 11:08:15 +0300

    Adding Splash Screen  (Additional Features)

---
+++ app/src/main/java/ise308/ozbakan/vahikutluhan/g16_carshowroomapp/SplashActivity.kt
@@ -0,0 +1,32 @@
+@file:Suppress("DEPRECATION")
+
+package ise308.ozbakan.vahikutluhan.g16_carshowroomapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.logo_splash)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            //finish this activity
+            finish()
+        },3000)
+
+    }
+}
